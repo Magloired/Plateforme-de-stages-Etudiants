1fbc0283686eacf8a0c44ca9e98eb5e27649f6d1
DIVO <mdivoayaovi@gmail.com>
2025-06-05 03:26:13 +0000

    Recréation du backend propre avec PostgreSQL

---
+++ backend/Models/Entreprise.cs
@@ -0,0 +1,7 @@
+public class Entreprise
+{
+    public int Id { get; set; }
+    public string? Nom { get; set; }
+    public string? Adresse { get; set; }
+    public string? EmailContact { get; set; }
+}
